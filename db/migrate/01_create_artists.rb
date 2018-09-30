89f5bad2aeaa10a846d6c4fffb1db2a75aeacf12
Michael Roose <mjroose@gmail.com>
2018-09-30 14:23:13 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,10 @@
 class CreateArtists < ActiveRecrod::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
